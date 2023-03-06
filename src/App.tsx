0fbcf75a96921ab1fab30dd1585b620699fb4a9c
吴海洋 <why@vip.local>
2023-03-06 20:23:02 +0800

    03

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link, NavLink } from "react-router-dom";
 import Course from "./pages/Course";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <BrowserRouter>
+      {/* 加入导航 */}
+      <header>
+        <nav>
+          <h1>导航</h1>
+          <NavLink to="/">主页</NavLink> | <NavLink to="/course">课程</NavLink>
+        </nav>
+      </header>
       <main>
         <Routes>
           <Route path="/" element={<Home />}></Route>
